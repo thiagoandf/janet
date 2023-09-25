355a49cc11f50db12bfd9c32ddffa2934ade8363
Thiago de Andrade Feliciano <thiagoandf@gmail.com>
2023-09-25 19:46:01 +0200

    replace all with Svelte

--- app/containers/App/actions.js
+++
@@ -1,8 +0,0 @@
-import { SET_BACKGROUND_POSITION } from 'containers/App/constants';
-
-export function setBackgroundPosition(position) {
-  return {
-    type: SET_BACKGROUND_POSITION,
-    position,
-  };
-}
